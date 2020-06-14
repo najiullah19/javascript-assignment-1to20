c2eafd10a3eb6cc8fd9f133d17add16a28da1b4c
najiullah19 <naji30744@gmail.com>
2020-06-15 01:04:21 +0500

    js assignment

---
+++ app.js
@@ -0,0 +1,28 @@
+
+// CHAPTER 1
+
+
+// TASK 2
+
+// // var a = "Error! Please enter a valid password" 
+// // alert(a)
+
+// TASK 3
+
+// var a = (" Wellcome to JS land..... \n Happy coding!");
+
+// alert(a)
+
+
+// TASK 4
+
+// var a = (" Wellcome to JS land....")
+//  var b = ("Happy Coding \n  prevent this page from creating additional dialog")
+
+//  alert(a)
+//  alert(b)
+
+// TASK 5
+
+// var a ="Hello... ican run JS through my web brower's console"
+// console.log(a)
